48cfb54625f4c5e643daca9726f78da9bd2e95b7
Mallika123allampati <104915981+Mallika123allampati@users.noreply.github.com>
2022-06-22 12:24:08 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>loginpage</title>
+        <link rel="stylesheet" type="text/css" href="main.css">
+</head>
+<body>
+    <h1>LOGIN FORM</h1>
+    <div class="container"><br> <br>
+        <form action="connect.php" method="POST">
+        <label>Username</label>
+        <input type="text" id="user" name="username"><br><br>
+        <label>Password</label>
+        <input type="password" id="pass" name="password"><br><br>
+        
+        <input type="submit" id="btn" value="submit">
+</form>
+</div>
+</body>
+</html>
